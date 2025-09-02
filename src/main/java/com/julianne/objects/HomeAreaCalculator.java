5efc493e9f1f89f0b5563c2d0106b20b3583161a
juli-anne <julianne.lazar@gmail.com>
2025-09-02 21:14:33 +0200

    expand the home area calculator with sending and reciving objects

--- src/main/java/com/julianne/objects/HomeAreaCalculator.java
+++ src/main/java/com/julianne/objects/HomeAreaCalculator.java
@@ -1,9 +1,19 @@
 package com.julianne.objects;
 
+import java.util.Scanner;
+
 public class HomeAreaCalculator {
+
+    private Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
 
+        HomeAreaCalculator calculator = new HomeAreaCalculator();
+        Rectangle room1 = calculator.getRoom();
+        Rectangle room2 = calculator.getRoom();
+        double totalArea = calculator.calculateArea(room1, room2);
+
+        /*
         // using the first constructor
         Rectangle room1 = new Rectangle();
         room1.setLength(50);
@@ -14,6 +24,21 @@ public class HomeAreaCalculator {
         double areOfRoom2 = room2.calculateArea();
 
         double totalArea = areOfRoom + areOfRoom2;
+        */
+
         System.out.println("Total Area: " + totalArea);
+    }
+
+    public Rectangle getRoom() {
+        System.out.println("Enter the length of the room");
+        double length = scanner.nextDouble();
+        System.out.println("Enter the width of the room");
+        double width = scanner.nextDouble();
+
+        return new Rectangle(length, width);
+    }
+
+    public double calculateArea(Rectangle room1, Rectangle room2) {
+        return room1.calculateArea() + room2.calculateArea();
     }
 }
