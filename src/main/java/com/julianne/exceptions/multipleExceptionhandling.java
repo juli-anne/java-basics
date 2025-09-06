11d0abeeca2220a8127b465daef280fa2db85709
juli-anne <julianne.lazar@gmail.com>
2025-09-06 18:47:58 +0200

    close the resource with finally

--- src/main/java/com/julianne/exceptions/multipleExceptionhandling.java
+++ src/main/java/com/julianne/exceptions/multipleExceptionhandling.java
@@ -10,23 +10,26 @@ public class multipleExceptionhandling {
     public static void main(String[] args) {
 
         File file = new File("files/numbers.txt");
+        Scanner fileReader = null;
         try {
-            Scanner fileReader = new Scanner(file);
+            fileReader = new Scanner(file);
             // read from the file
             while (fileReader.hasNext()) {
                 System.out.println(fileReader.nextDouble()); // throws an exception because expected double
             }
-          } catch (FileNotFoundException | InputMismatchException e ) {
-            e.printStackTrace(); // catch in a single line
-        }
-
-//        } catch (FileNotFoundException e) {
-//            System.out.println("File not found exception: " + e.getMessage());
-//        } catch (InputMismatchException e) {
-//            System.out.println("Input Mismatch exception: " + e.getMessage());
-//        } catch (Exception e) {
-//            throw new RuntimeException(e);
+//          } catch (FileNotFoundException | InputMismatchException e ) {
+//            e.printStackTrace(); // catch in a single line
 //        }
 
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found exception: " + e.getMessage());
+        } catch (InputMismatchException e) {
+            System.out.println("Input Mismatch exception: " + e.getMessage());
+        } catch (Exception e) {
+            System.out.println("Unexpected exception: " + e.getMessage());
+        } finally {
+            assert fileReader != null;
+            fileReader.close();
+        }
     }
 }
