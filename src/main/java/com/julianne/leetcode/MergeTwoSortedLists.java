1d69db39a9d077a74f66325a809bc895a5e399a4
juli-anne <julianne.lazar@gmail.com>
2025-09-18 12:49:56 +0200

    practice the Merge Two Sorted Lists task

---
+++ src/main/java/com/julianne/leetcode/MergeTwoSortedLists.java
@@ -0,0 +1,62 @@
+package com.julianne.leetcode;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+public class MergeTwoSortedLists {
+
+    //  definition for singly-linked list
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null) return list2;
+        if (list2 == null) return list1;
+
+        if (list1.val <= list2.val) {
+            list1.next = mergeTwoLists(list1.next, list2);
+            return list1;
+        } else {
+            list2.next = mergeTwoLists(list1, list2.next);
+            return list2;
+        }
+    }
+
+    // helper method to print the linked list
+    public static void printList(ListNode head) {
+        while (head != null) {
+            System.out.print(head.val);
+            if (head.next != null) System.out.print(" -> ");
+            head = head.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        MergeTwoSortedLists solution = new MergeTwoSortedLists();
+
+        // build first list: 1 -> 2 -> 4
+        ListNode list1 = new ListNode(1, new ListNode(2, new ListNode(4)));
+
+        // build second list: 1 -> 3 -> 4
+        ListNode list2 = new ListNode(1, new ListNode(3, new ListNode(4)));
+
+        // merge lists
+        ListNode merged = solution.mergeTwoLists(list1, list2);
+
+        // print result
+        printList(merged); // output: 1 -> 1 -> 2 -> 3 -> 4 -> 4
+    }
+}
